6d1c07cce80c443a0c4d6fe40bf7ecd8f0ca2e52
陈静 <205284640@qq.com>
2020-09-03 22:05:56 +0800

    添加CSphere类

--- PathTracing/CSphere.cpp
+++ PathTracing/CSphere.cpp
@@ -1,1 +1,26 @@
 #include "CSphere.h"
+
+
+CSphere::CSphere(float vRadius, vec3 vCenter, SMaterial vMaterial)
+{
+	m_Radius = vRadius;
+	m_Center = vCenter;
+	m_Material.Absorption = vMaterial.Absorption;
+	m_Material.Color = vMaterial.Color;
+	m_Material.Disperse = vMaterial.Disperse;
+	m_Material.Reflection = vMaterial.Reflection;
+}
+
+const float CSphere::getRadius() const
+{
+	return m_Radius;
+}
+
+const vec3 CSphere::getColor() const
+{
+	return m_Material.Color;
+}
+const vec3 CSphere::getCenter() const
+{
+	return m_Center;
+}
--- PathTracing/CSphere.h
+++ PathTracing/CSphere.h
@@ -1,5 +1,27 @@
 #pragma once
+#include<glm/glm.hpp>
+using namespace glm;
+
+
 class CSphere
 {
+	struct SMaterial
+	{
+		float Absorption;
+		float Disperse;
+		float Reflection;
+		vec3 Color;
+	};
+
+	CSphere(float vRadius, vec3 vCenter, SMaterial vMaterial);
+	const float getRadius() const;
+	const vec3 getColor() const;
+	const vec3 getCenter() const;
+
+private:
+	vec3 m_Center;
+	float m_Radius;
+	SMaterial m_Material;
+
 };
 
